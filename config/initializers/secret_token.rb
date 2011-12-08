ad094e328bbf3d92a437d2dfce5b219640b61c0b
leon <leon@boldmovestudio.com>
2011-12-08 10:37:36 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToteApp::Application.config.secret_token = '06aa46f0ad3698676093faf5604ce583304f250872ea9da6eb3a649c02f6dccb86ec1bbc00ec8e68053ea8c7df1b65a96358bcb57d74cc28d169e831e9e93e07'
