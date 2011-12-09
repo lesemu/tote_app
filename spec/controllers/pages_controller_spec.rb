6d2525e0b84756a912196a6a605086aa40d22332
leon <leon@boldmovestudio.com>
2011-12-08 22:09:28 -0400

    test testing

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    
+    it "should have the right title" do 
+    	get 'home'
+    	response.should have_selector("title",
+    						:content => "A way to declutter | Home")
     end
   end
 
@@ -14,6 +21,24 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    
+    it "should have the right title" do 
+    	get 'contact'
+    	response.should have_selector("title",
+    						:content => "A way to declutter | Contact")
+    end
   end
 
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do 
+    	get 'about'
+    	response.should have_selector("title",
+    						:content => "A way to declutter | About")
+    end
+  end
 end
